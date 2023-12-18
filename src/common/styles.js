ea15e630bb5dd53fe09229690af9bc7aa8a82bf4
Arun Joseph <arunjoseph3007@gmail.com>
2023-12-18 17:13:38 +0530

    Colors system

--- src/common/styles.js
+++ src/common/styles.js
@@ -5,3 +5,9 @@ const { height, width } = Dimensions.get("window");
 export const vh = (val) => (val * height) / 100;
 
 export const vw = (val) => (val * width) / 100;
+
+export const Colors = {
+  black: "#000",
+  white: "#fff",
+  green: "#23d997",
+};
