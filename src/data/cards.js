d02ba7b994053f487152d0b7458d720029a175a8
Arun Joseph <arunjoseph3007@gmail.com>
2023-12-19 10:39:41 +0530

    Cards logic implemented

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,71 +1,4 @@
-const Companies = [
-  { id: 1, name: "Tata", startingPrice: 35, maxCardVal: 15 },
-  { id: 2, name: "ONGC", startingPrice: 40, maxCardVal: 15 },
-  { id: 3, name: "Reliance", startingPrice: 50, maxCardVal: 20 },
-  { id: 4, name: "Infosys", startingPrice: 55, maxCardVal: 20 },
-  { id: 5, name: "SBI", startingPrice: 60, maxCardVal: 20 },
-  { id: 6, name: "Reliance Industries", startingPrice: 80, maxCardVal: 25 },
-  { id: 7, name: "Nifty", startingPrice: 120, maxCardVal: 30 },
-];
-
-const Cards = [];
-
-Companies.forEach(function (company) {
-  for (let i = 5; i <= company.maxCardVal; i += 5) {
-    Cards.push({
-      type: "NORMAL",
-      companyId: company.id,
-      netChange: i,
-    });
-    Cards.push({
-      type: "NORMAL",
-      companyId: company.id,
-      netChange: i,
-    });
-  }
-
-  for (let i = -5; i >= -company.maxCardVal; i -= 5) {
-    Cards.push({
-      type: "NORMAL",
-      companyId: company.id,
-      netChange: i,
-    });
-    Cards.push({
-      type: "NORMAL",
-      companyId: company.id,
-      netChange: i,
-    });
-  }
-});
-
-const CrystalCards = [
-  { type: "CRYSTAL", crystalType: "FRAUD" },
-  { type: "CRYSTAL", crystalType: "DIVIDEND" },
-  { type: "CRYSTAL", crystalType: "BONUS_SHARE" },
-  { type: "CRYSTAL", crystalType: "RIGHT_ISSUE" },
-  { type: "CRYSTAL", crystalType: "LOAN_ON_STOCK" },
-];
-
-CrystalCards.forEach((card) => {
-  Cards.push(card);
-  Cards.push(card);
-});
-
-const circuitDenomination = [5, 10, 20];
-
-circuitDenomination.forEach((denom) => {
-  Cards.push({
-    type: "CIRCUIT",
-    circuitType: "UP",
-    denomination: denom,
-  });
-  Cards.push({
-    type: "CIRCUIT",
-    circuitType: "LOW",
-    denomination: denom,
-  });
-});
-
+/*
 const gameState = {
   comapnyValues: {
     companyId: companyShareValue,
@@ -79,62 +12,195 @@ const gameState = {
     },
   },
 };
+*/
 
+export const Companies = [
+  { id: 1, name: "Tata", startingPrice: 35, maxCardVal: 15 },
+  { id: 2, name: "ONGC", startingPrice: 40, maxCardVal: 15 },
+  { id: 3, name: "Reliance", startingPrice: 50, maxCardVal: 20 },
+  { id: 4, name: "Infosys", startingPrice: 55, maxCardVal: 20 },
+  { id: 5, name: "SBI", startingPrice: 60, maxCardVal: 20 },
+  { id: 6, name: "Reliance Industries", startingPrice: 80, maxCardVal: 25 },
+  { id: 7, name: "Nifty", startingPrice: 120, maxCardVal: 30 },
+];
 
+export function initializeGameState(noOfPlayers) {
+  const gameState = {
+    comapnyValues: {},
+    userState: {},
+  };
 
-function applyCard(initial, final, card, userId, comapnyId = null, amountSpent = null) {
+  Companies.forEach((company) => {
+    gameState.comapnyValues[company.id] = company.startingPrice;
+  });
+
+  for (let i = 0; i < noOfPlayers; i++) {
+    gameState.userState[i] = {
+      cashInHand: 800000,
+      holdings: {},
+    };
+    Companies.forEach((company) => {
+      gameState.userState[i].holdings[company.id] = 0;
+    });
+  }
+
+  return gameState;
+}
+
+export function getCardStack() {
+  const Cards = [];
+
+  Companies.forEach(function (company) {
+    for (let i = 5; i <= company.maxCardVal; i += 5) {
+      Cards.push({
+        type: "NORMAL",
+        companyId: company.id,
+        netChange: i,
+      });
+      Cards.push({
+        type: "NORMAL",
+        companyId: company.id,
+        netChange: i,
+      });
+    }
+
+    for (let i = -5; i >= -company.maxCardVal; i -= 5) {
+      Cards.push({
+        type: "NORMAL",
+        companyId: company.id,
+        netChange: i,
+      });
+      Cards.push({
+        type: "NORMAL",
+        companyId: company.id,
+        netChange: i,
+      });
+    }
+  });
+
+  const CrystalCards = [
+    { type: "CRYSTAL", crystalType: "FRAUD" },
+    { type: "CRYSTAL", crystalType: "DIVIDEND" },
+    { type: "CRYSTAL", crystalType: "BONUS_SHARE" },
+    { type: "CRYSTAL", crystalType: "RIGHT_ISSUE" },
+    { type: "CRYSTAL", crystalType: "LOAN_ON_STOCK" },
+  ];
+
+  CrystalCards.forEach((card) => {
+    Cards.push(card);
+    Cards.push(card);
+  });
+
+  const circuitDenomination = [5, 10, 20];
+
+  circuitDenomination.forEach((denom) => {
+    Cards.push({
+      type: "CIRCUIT",
+      circuitType: "UP",
+      denomination: denom,
+    });
+    Cards.push({
+      type: "CIRCUIT",
+      circuitType: "LOW",
+      denomination: denom,
+    });
+  });
+
+  return Cards;
+}
+
+export function applyCard(
+  initial,
+  final,
+  card,
+  userId,
+  companyId = null,
+  amountSpent = null
+) {
+  const copy = JSON.parse(JSON.stringify(final));
+
   switch (card.type) {
     case "NORMAL": {
-      final.comapnyValues[card.companyId] += card.netChange;
-      return final;
+      copy.comapnyValues[card.companyId] += card.netChange;
+      return copy;
     }
     case "CIRCUIT": {
       switch (card.circuitType) {
         case "UP": {
-          final.comapnyValues[comapnyId] = Math.min(
+          copy.comapnyValues[comapnyId] = Math.min(
             final.comapnyValues[comapnyId],
             initial.comapnyValues[comapnyId] + card.denomination
           );
-          return final;
+          return copy;
         }
         case "LOW": {
-          final.comapnyValues[comapnyId] = Math.max(
+          copy.comapnyValues[comapnyId] = Math.max(
             final.comapnyValues[comapnyId],
             initial.comapnyValues[comapnyId] - card.denomination
           );
-          return final;
+          return copy;
+        }
+        default: {
+          throw new Error("Circuit type not identified");
         }
       }
     }
     case "CRYSTAL": {
       switch (card.crystalType) {
         case "FRAUD": {
-          if (amountSpent<final.userState[userId].cashInHand){
-          final.userState[userId].cashInHand-=amountSpent
-          final.userState[userId].holdings[companyId]+=amountSpent*2/final.comapnyValues[comapnyId]
+          if (amountSpent < final.userState[userId].cashInHand) {
+            copy.userState[userId].cashInHand -= amountSpent;
+            copy.userState[userId].holdings[companyId] +=
+              (amountSpent * 2) / final.comapnyValues[comapnyId];
           }
-          return final;
+          return copy;
         }
         case "DIVIDEND": {
-          final.userState[userId].cashInHand+=final.userState[userId].holdings[companyId]*5
-          return final;
+          copy.userState[userId].cashInHand +=
+            final.userState[userId].holdings[companyId] * 5;
+          return copy;
         }
         case "BONUS_SHARE": {
-          final.userState[userId].holdings[companyId]+=Math.round(final.userState[userId].holdings[companyId]/5000)*1000
-          return final;
+          copy.userState[userId].holdings[companyId] +=
+            Math.round(final.userState[userId].holdings[companyId] / 5000) *
+            1000;
+          return copy;
         }
         case "RIGHT_ISSUE": {
-          final.userState[userId].cashInHand-=10*Math.round(final.userState[userId].holdings[companyId]/2000)*1000
-          final.userState[userId].holdings[companyId]+=Math.round(final.userState[userId].holdings[companyId]/2000)*1000
-          return final;
+          const noOfStocks =
+            Math.round(final.userState[userId].holdings[companyId] / 2000) *
+            1000;
+
+          if (copy.userState[userId].cashInHand >= 10 * noOfStocks) {
+            copy.userState[userId].cashInHand -= 10 * noOfStocks;
+            copy.userState[userId].holdings[companyId] += noOfStocks;
+          }
+          return copy;
         }
         case "LOAN_ON_STOCK": {
-          final.userState[userId].cashInHand+=100000
-          return final;
+          copy.userState[userId].cashInHand += 100000;
+          return copy;
         }
       }
+    }
+    default: {
+      throw new Error("Card type not identified");
     }
   }
 }
 
-console.log(Cards);
+export function getShuffledCards(rounds = 3) {
+  const cards = getCardStack();
+  const length = cards.length;
+
+  for (let j = 0; j < rounds; j++) {
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * length);
+
+      const temp = cards[i];
+      cards[i] = cards[randomIndex];
+      cards[randomIndex] = temp;
+    }
+  }
+  return cards;
+}
