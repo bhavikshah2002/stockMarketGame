7795b25b6aabe7873a58e6181169f1d4e060f741
Arun Joseph <arunjoseph3007@gmail.com>
2023-12-20 10:21:20 +0530

    Common roon initial

--- app/gameroom/index.js
+++ app/gameroom/index.js
@@ -1,12 +1,35 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { RegularText } from "../../src/common/Text";
 
 export default function CommonRound() {
-    return (
-        <View>
-            <RegularText>
-                Common Round Page
-            </RegularText>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.sides}>
+        <RegularText>Left</RegularText>
+      </View>
+      <View style={styles.middle}>
+        <RegularText>Mid</RegularText>
+      </View>
+      <View style={styles.sides}>
+        <RegularText>Right</RegularText>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 4,
+  },
+
+  sides: {
+    flex: 1,
+  },
+
+  middle: {
+    flex: 1,
+  },
+});
