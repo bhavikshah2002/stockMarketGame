1e64998542c7907e962725d82fb60095fd07f819
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-12-22 18:09:58 +0530

    Price Book Modal Design Done

--- src/components/PriceBook.js
+++ src/components/PriceBook.js
@@ -1,18 +1,40 @@
 import { TouchableOpacity, View } from "react-native";
 import { Modal } from "react-native";
 import { StyleSheet } from "react-native";
-import { BoldText, RegularText } from "../common/Text";
+import { BoldText, RegularText, SemiBoldText } from "../common/Text";
 import { Colors } from "../common/styles";
-
-function getRow({values}) {
-    return(
-        <View>
-            <RegularText>Round</RegularText>
-        </View>
-    )
-}
+import {
+  Col,
+  Row,
+  Rows,
+  Table,
+  TableWrapper,
+} from "react-native-table-component";
 
 export default function PriceBook({ priceBookVisible, setPriceBookVisible }) {
+  const header = [
+    "Tata",
+    "ONGC",
+    "Reliance",
+    "Infosys",
+    "SBI",
+    "Adani",
+    "Nifty",
+  ];
+
+  const data = [
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],
+  ];
   return (
     <Modal
       animationType="slide"
@@ -25,20 +47,34 @@ export default function PriceBook({ priceBookVisible, setPriceBookVisible }) {
       <View style={styles.Container}>
         <View style={styles.InnerConatiner}>
           <View>
-            <BoldText size={25} style={{textDecorationLine:"underline",fontStyle:"italic"}}>
-                Price Book
-            </BoldText>
-            <View>
-                
-            </View>
+            <RegularText
+              size={25}
+              style={{ textDecorationLine: "underline"}}
+            >
+              Price Book
+            </RegularText>
+          </View>
+          <View style={styles.TableConatiner}>
+            <Table borderStyle={{ borderWidth: 0.2 ,borderColor:"white"}}>
+              <Row
+                data={header}
+                style={styles.head}
+                textStyle={styles.textHeading}
+              />
+              <Rows
+                data={data}
+                style={styles.row}
+                textStyle={styles.textContent}
+              />
+            </Table>
           </View>
           <View>
             <TouchableOpacity
-              style={{}}
+              style={styles.Btn}
               onPress={() => setPriceBookVisible(!priceBookVisible)}
             >
-              <BoldText size={25} transform="uppercase">
-                Go Back
+              <BoldText size={15} transform="uppercase" color={Colors.dim}>
+                Close
               </BoldText>
             </TouchableOpacity>
           </View>
@@ -50,17 +86,45 @@ export default function PriceBook({ priceBookVisible, setPriceBookVisible }) {
 
 const styles = StyleSheet.create({
   Container: {
-    flex:1,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   InnerConatiner: {
-    height:"100%",
-    width: "50%",
+    height: "100%",
+    width: "70%",
     backgroundColor: "#262525",
-    position:"absolute",
-    right:0,
-    justifyContent: "center",
+    position: "absolute",
+    right: 0,
+    justifyContent: "space-between",
     alignItems: "center",
+    paddingVertical: 20,
+  },
+  TableConatiner: {
+    width: 450,
+  },
+  head: {
+    height: 30,
+    backgroundColor: Colors.dim,
+  },
+  row: {
+    height: 20,
+  },
+  textHeading: {
+    textAlign: "center",
+    fontWeight: "800",
+  },
+  textContent: {
+    textAlign: "center",
+    color: Colors.white,
+  },
+  Btn: {
+    height: 40,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth:3,
+    borderColor:Colors.dim,
+    borderRadius:7
   },
 });
