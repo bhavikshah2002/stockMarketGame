f2e1a55b38c2d373a35dd2dd96e9f7374a0e82fb
Arun Joseph <arunjoseph3007@gmail.com>
2023-12-23 12:55:00 +0530

    Animation started

--- src/components/RoundStartScreen.js
+++ src/components/RoundStartScreen.js
@@ -1,17 +1,42 @@
 import { Dimensions, StyleSheet, View } from "react-native";
 import { Colors } from "../common/styles";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 const { height } = Dimensions.get("window");
 const padding = 10;
 const radius = (height - 2 * padding) / 2;
 
 export default function RoundStartScreen() {
+  const scale = useSharedValue(0);
+  const duration = 4000;
+  const easing = Easing.cubic;
+
+  useEffect(() => {
+    scale.value = withRepeat(
+      withSequence(
+        withTiming(0, { duration, easing }),
+        withTiming(1, { duration, easing }),
+        withTiming(0, { duration, easing })
+      ),
+      -1,
+      true
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.sqContainer}>
         <View style={styles.circleTable}>
-          {new Array(8).fill(0).map((_, i, arr) => (
-            <Card index={i} totalDecks={arr.length} />
+          {new Array(7).fill(0).map((_, i, arr) => (
+            <Card scale={scale} index={i} totalDecks={arr.length} />
           ))}
         </View>
       </View>
@@ -19,25 +44,49 @@ export default function RoundStartScreen() {
   );
 }
 
-function Card({ totalDecks, index, children }) {
+function Card({ totalDecks, index, children, scale }) {
   const cardHeight = 40;
   const cardWidth = 30;
   const angle = (2 * Math.PI * index) / totalDecks;
 
-  return (
-    <View
-      style={{
-        height: cardHeight,
-        width: cardWidth,
+  const animatedStyles = useAnimatedStyle(() => {
+    const overlap = 0.5;
+    const isActive =
+      scale.value * totalDecks > index - overlap &&
+      scale.value * totalDecks < index + 1 + overlap;
+
+    if (!isActive) {
+      return {
+        bottom: radius - cardHeight / 2,
+        right: radius - cardWidth / 2,
         backgroundColor: Colors.dim,
-        borderRadius: 3,
-        position: "absolute",
-        bottom: 0.8 * radius * Math.sin(angle) + radius - cardHeight / 2,
-        right: 0.8 * radius * Math.cos(angle) + radius - cardWidth / 2,
-      }}
+      };
+    }
+
+    const scalefactor = scale.value * radius;
+
+    return {
+      bottom: scalefactor * Math.sin(angle) + radius - cardHeight / 2,
+      right: scalefactor * Math.cos(angle) + radius - cardWidth / 2,
+      backgroundColor: Colors.teal,
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          height: cardHeight,
+          width: cardWidth,
+          backgroundColor: Colors.dim,
+          borderRadius: 3,
+          position: "absolute",
+        },
+        animatedStyles,
+      ]}
     >
       {children}
-    </View>
+    </Animated.View>
   );
 }
 
