ec7469900d4ce8ec9e008406b855f8dbbdd175bd
Arun Joseph <arunjoseph3007@gmail.com>
2024-01-15 17:54:42 +0530

    Socket class implemented

---
+++ src/utils/socket.js
@@ -0,0 +1,31 @@
+export default class SocketConn {
+  constructor(url) {
+    this.url = url;
+    this.conn = new WebSocket(this.url);
+    this.listeners = [];
+
+    this.conn.onmessage((ev) => {
+      const data = JSON.parse(ev.data);
+
+      this.listeners
+        .filter((listener) => listener.type == ev.type)
+        .forEach((listener) => {
+          listener.callback(data, ev);
+        });
+    });
+  }
+
+  on(event, callback) {
+    this.listeners.push({ event, callback });
+  }
+
+  off(event, callback) {
+    this.listeners = this.listeners.filter(
+      (listener) => listener.event != event && listener.callback != callback
+    );
+  }
+
+  emit(type, data) {
+    this.conn.send(JSON.stringify({ type, data }));
+  }
+}
