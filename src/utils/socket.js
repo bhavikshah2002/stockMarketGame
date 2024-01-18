8bef2936a2e030e21b33dcf67b94854c90bfe17d
Arun Joseph <arunjoseph3007@gmail.com>
2024-01-18 12:39:37 +0530

    Join integration

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -14,7 +14,7 @@ export default class SocketConn {
     };
     this.conn.onmessage = (ev) => {
       const data = JSON.parse(ev.data);
-      console.log(data.data);
+      console.log(data);
       this.listeners
         .filter((listener) => listener.type == data.type)
         .forEach((listener) => {
